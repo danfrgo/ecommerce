bcb79246394ff6faa8d633430f727ba9a6d7f4a6
danfrgo <danielfr_goncalves@hotmail.com>
2021-01-20 18:58:57 +0000

    ProductsController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,7 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Controllers
 {
-    public class ProductsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
     {
-        
+        [HttpGet]
+        public string GetProducts(){
+            return "vai retornar uma lista de produtos";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id){
+            return "retorna um so produto";
+        }
+
+
+
+
+
     }
 }
