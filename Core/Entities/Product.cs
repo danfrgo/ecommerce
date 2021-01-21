541a10dac98e85c939092a8abf9f1fd3063db231
danfrgo <danielfr_goncalves@hotmail.com>
2021-01-21 15:59:24 +0000

    Add novas classes e migrations

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,8 +1,19 @@
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
+        //
+        // o Id do produto será herdado do BaseEntity
+        //
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureURL { get; set; }
+        
+        // relationships
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
     }
 }
