64a4172d6867ba61ae4d263febe31d4f6dbfabde
danfrgo <danielfr_goncalves@hotmail.com>
2021-02-16 15:20:48 +0000

    Criar specification class

---
+++ Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<T, bool>> Criteria {get;}
+
+        // vai dar uma lista dos includes necessarios
+        public List<Expression<Func<T, object>>> Includes {get;} = new List<Expression<Func<T, object>>>();
+
+        // metodo para add include statements à lista de cima
+        protected void  AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+
+
+
+
+    }
+}
